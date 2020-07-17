81456c9ae50c2c33d4963dad4222539569305992
dlsrb6342 <dlsrb6342@gmail.com>
2020-07-18 00:14:26 +0900

    Add api project

---
+++ boss-api/src/test/java/gelato/riso/bossapi/BossApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gelato.riso.bossapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BossApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
